4e176d44d23bf9f495175b4830bc3ef6a1e70354
Narendranath Gogineni <narenrockstar1@gmail.com>
2025-08-18 21:39:51 +0530

    add selected node drawer

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ export default function App() {
     <div id="main-container" style={{height: "100vh", width: "100vw"}}>
       <SearchBox searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
       <Map searchQuery={searchQuery} selectedBook={selectedBook} setSelectedBook={setSelectedBook} />
-      {/* <BookDetailsDrawer selectedBook={selectedBook} setSelectedBook={setSelectedBook} /> */}
+      <BookDetailsDrawer selectedBook={selectedBook} setSelectedBook={setSelectedBook} />
     </div>
   );
 }
--- src/components/BookDetailsDrawer.tsx
+++ src/components/BookDetailsDrawer.tsx
@@ -1,22 +1,25 @@
-export default function BookDetailsDrawer({ selectedBook, setSelectedBook }: { selectedBook: string | null, setSelectedBook: (book: string | null) => void }) {
+import { MapGeoJSONFeature } from "maplibre-gl";
+import './BookDetailsDrawer.css';
+
+export default function BookDetailsDrawer({ selectedBook, setSelectedBook }: { selectedBook: MapGeoJSONFeature | null, setSelectedBook: (book: MapGeoJSONFeature | null) => void }) {
   if (!selectedBook) return null;
   
-  const goodreadsUrl = `https://www.goodreads.com/book/show/${selectedBook}`;
+  const goodreadsUrl = `https://www.goodreads.com/search?q=${selectedBook.properties?.title}`;
   
   return (
-    <div className="drawer-container">
+    <div className={`drawer-container ${selectedBook ? 'open' : ''}`}>
       <div className="drawer-header">
         <button onClick={() => setSelectedBook(null)}>Close</button>
       </div>
       <div className="drawer-content">
-        <iframe
-          src={goodreadsUrl}
-          title="Goodreads Book"
-          width="100%"
-          height="800px"
-          frameBorder="0"
-          allowFullScreen
-        />
+        <h2>{selectedBook.properties?.title}</h2>
+        {selectedBook.properties?.author_names && <p>Author(s): {selectedBook.properties?.author_names}</p>}
+        {selectedBook.properties?.average_rating && <p>Average Rating: {selectedBook.properties?.average_rating}</p>}
+        {selectedBook.properties?.publication_year && <p>Publication Year: {selectedBook.properties?.publication_year}</p>}
+        {selectedBook.properties?.publisher && <p>Publisher: {selectedBook.properties?.publisher}</p>}
+        {selectedBook.properties?.description && <p>Description: {selectedBook.properties?.description}</p>}
+        {selectedBook.properties?.id && <p>Goodreads ID: {selectedBook.properties?.id}</p>}
+        {selectedBook.properties?.id && <p>Goodreads URL: <a href={goodreadsUrl} target="_blank" rel="noopener noreferrer">{goodreadsUrl}</a></p>}
       </div>
     </div>
   );
